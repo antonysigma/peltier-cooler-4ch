1ac6c1d7c10ed6d453f54f234ba15b22651ae169
Antony Chan <antony.sigma@gmail.com>
2023-05-02 08:20:18 -0700

    Display logic in struct

--- firmware/display.cpp
+++ firmware/display.cpp
@@ -1,74 +1,65 @@
 #include "display.h"
 
-#include <Adafruit_SSD1306.h>
-#include <Arduino.h>
-#include <SPI.h>
-#include <Wire.h>
-
 #include "config.h"
 
-namespace {
-
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
-}
+Display::Display() : handle{SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET} {}
 
 void
-setupDisplay() {
-    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3D))
-        Serial.println("Display failed");
+Display::setup() {
+    if (!handle.begin(SSD1306_SWITCHCAPVCC, 0x3D))
+        Serial.println(F("handle failed"));
     else {
-        display.display();
+        handle.display();
         delay(2000);
-        display.clearDisplay();
+        handle.clearDisplay();
     }
 }
 
 void
-displayTemp(const unsigned long &currentTime, const float &tempA, const int &controlPWMA,
-            const float &tempB, const int &controlPWMB) {
+Display::print(const unsigned long currentTime, const float tempA, const int controlPWMA,
+               const float tempB, const int controlPWMB) {
     const unsigned long wait = 500;
     static unsigned long previousTime = millis();
     if (currentTime - previousTime < wait) return;
     previousTime = currentTime;
 
-    display.clearDisplay();
+    handle.clearDisplay();
 
-    display.setTextSize(1);
-    display.setTextColor(WHITE);
-    display.setCursor(0, 0);
-    display.println("Peltier controller");
+    handle.setTextSize(1);
+    handle.setTextColor(WHITE);
+    handle.setCursor(0, 0);
+    handle.println("Peltier controller");
 
-    // display.setTextSize (1);
-    display.print("T=  ");
+    // handle.setTextSize (1);
+    handle.print("T=  ");
     if (tempA > 0.0f) {
-        display.print(tempA);
-        display.print("C  ");
+        handle.print(tempA);
+        handle.print("C  ");
     } else
-        display.print("ERR");
+        handle.print("ERR");
 
     if (tempB > 0.0f) {
-        display.print(tempB);
-        display.println("C");
+        handle.print(tempB);
+        handle.println("C");
     } else
-        display.println("ERR");
+        handle.println("ERR");
 
-    display.print("TEC=");
-    display.print(controlPWMA * 100 / 255);
-    display.print("%     ");
-    display.print(controlPWMB * 100 / 255);
-    display.println("%");
+    handle.print("TEC=");
+    handle.print(controlPWMA * 100 / 255);
+    handle.print("%     ");
+    handle.print(controlPWMB * 100 / 255);
+    handle.println("%");
 
-    display.setTextSize(2);
+    handle.setTextSize(2);
     if (digitalRead(lockLED) == HIGH) {
-        display.println("LOCKED");
+        handle.println("LOCKED");
         digitalWrite(alarmLED, LOW);
     } else if (controlPWMA >= 254 || controlPWMB >= 254 || tempA <= 0.0f || tempB <= 0.0f) {
         digitalWrite(alarmLED, HIGH);
-        display.println("ALARM");
+        handle.println("ALARM");
     } else {
         digitalWrite(alarmLED, LOW);
     }
 
-    display.display();
+    handle.display();
 }
--- firmware/display.h
+++ firmware/display.h
@@ -1,6 +1,19 @@
 #pragma once
 
-void setupDisplay();
+#include <Adafruit_SSD1306.h>
+#include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
 
-void displayTemp(const unsigned long &currentTime, const float &tempA, const int &controlPWMA,
-                 const float &tempB, const int &controlPWMB);
+class Display {
+    Adafruit_SSD1306 handle;
+
+   public:
+    Display();
+    Display(const Display &) = delete;
+    Display(Display &&) = delete;
+
+    void setup();
+    void print(const unsigned long currentTime, const float tempA, const int controlPWMA,
+               const float tempB, const int controlPWMB);
+};
